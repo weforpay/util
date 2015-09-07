a5a1c413ffb674c272240d8d7f691c5af8cc5928
wwenhui <wwenhui@weforpay.com>
2015-09-07 17:17:38 +0800

    add ByteArray in util

---
+++ util/byte_array.go
@@ -0,0 +1,260 @@
+package util
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"math"
+)
+
+var ErrTooLarge = errors.New("bytes.Buffer: too large")
+var ErrOutOfRange = errors.New("out of range")
+
+type ByteArray struct {
+	buf       []byte
+	bootstrap [64]byte
+	position  int
+	byteOrder binary.ByteOrder
+}
+
+func NewByteArray() *ByteArray {
+	obj := &ByteArray{
+		byteOrder: binary.LittleEndian,
+	}
+
+	return obj
+}
+func (this *ByteArray) SetPosition(position int) (err error) {
+	if position >= len(this.buf) {
+		err = ErrOutOfRange
+	}
+	this.position = position
+	return
+}
+func (this *ByteArray) SetOrder(byteOrder binary.ByteOrder) {
+	this.byteOrder = byteOrder
+}
+func (this *ByteArray) Length() int {
+	return len(this.buf)
+}
+func (this *ByteArray) checkRange(size int) (err error) {
+	if (this.position + size) > len(this.buf) {
+		err = ErrOutOfRange
+	}
+	return
+}
+func (this *ByteArray) ReadBoolean() (v bool, err error) {
+	err = this.checkRange(1)
+	if err != nil {
+		return
+	}
+	if this.buf[this.position] > 0 {
+		v = true
+	} else {
+		v = false
+	}
+	this.position++
+	return
+}
+func (this *ByteArray) ReadByte() (v byte, err error) {
+	err = this.checkRange(1)
+	if err != nil {
+		return
+	}
+	if this.position < len(this.buf) {
+		v = this.buf[this.position]
+	}
+	this.position++
+	return
+}
+
+func (this *ByteArray) ReadBytes(other *ByteArray, offset int, length int) (err error) {
+	err = this.checkRange(length)
+	if err != nil {
+		return
+	}
+	if offset >= len(this.buf) {
+		err = fmt.Errorf("out of range")
+		return
+	}
+	l := len(this.buf)
+	if length == 0 {
+		length = l - offset
+	}
+	this.position = offset
+	for ; this.position < l && length > 0; this.position++ {
+		other.WriteByte(this.buf[this.position])
+		length--
+	}
+	return
+}
+
+func (this *ByteArray) ReadDouble() (v float64, err error) {
+	err = this.checkRange(8)
+	if err != nil {
+		return
+	}
+	u64 := this.byteOrder.Uint64(this.buf[this.position:])
+	v = math.Float64frombits(u64)
+	this.position += 8
+	return
+}
+
+func (this *ByteArray) ReadFloat() (v float32, err error) {
+	err = this.checkRange(4)
+	if err != nil {
+		return
+	}
+	u32 := this.byteOrder.Uint32(this.buf[this.position : this.position+4])
+	v = math.Float32frombits(u32)
+	this.position += 4
+	return
+}
+func (this *ByteArray) ReadInt() (v int32, err error) {
+	err = this.checkRange(4)
+	if err != nil {
+		return
+	}
+	v = int32(this.byteOrder.Uint32(this.buf[this.position : this.position+4]))
+	this.position += 4
+	return
+}
+func (this *ByteArray) ReadShort() (v int16, err error) {
+	err = this.checkRange(2)
+	if err != nil {
+		return
+	}
+	v = int16(this.byteOrder.Uint16(this.buf[this.position : this.position+2]))
+	this.position += 2
+	return
+}
+
+func (this *ByteArray) ReadUnsignedByte() (v uint8, err error) {
+	err = this.checkRange(1)
+	if err != nil {
+		return
+	}
+	v = uint8(this.buf[this.position])
+	this.position++
+	return
+}
+
+func (this *ByteArray) ReadUnsignedInt() (v uint32, err error) {
+	vint, err := this.ReadInt()
+	if err != nil {
+		return
+	}
+	v = uint32(vint)
+	return
+}
+
+func (this *ByteArray) ReadUnsignedShort() (v uint16) {
+	vshort, err := this.ReadShort()
+	if err != nil {
+		return
+	}
+	v = uint16(vshort)
+	return
+}
+
+func (this *ByteArray) WriteBoolean(v bool) {
+	var b byte
+	if v {
+		b = 1
+	} else {
+		b = 0
+	}
+
+	this.grow(1)
+	this.buf[this.position] = b
+	this.position++
+	return
+}
+func (this *ByteArray) WriteByte(v byte) {
+	this.grow(1)
+	this.buf[this.position] = v
+	this.position++
+	return
+}
+func (this *ByteArray) WriteBytes(other *ByteArray, offset int, length int) {
+	ol := len(other.buf)
+	if offset >= ol {
+		offset = 0
+	}
+	if offset+length >= ol {
+		length = ol - offset
+	}
+	if length == 0 {
+		length = ol
+	}
+	other.position = offset
+	for other.position < ol {
+		this.grow(1)
+		this.buf[this.position] = other.buf[other.position]
+		this.position++
+		other.position++
+		length--
+	}
+	return
+}
+func (this *ByteArray) WriteDouble(v float64) {
+	u64 := math.Float64bits(v)
+	this.grow(8)
+	this.byteOrder.PutUint64(this.buf[this.position:], u64)
+	this.position += 8
+	return
+}
+
+func (this *ByteArray) WriteFloat(v float32) {
+	u32 := math.Float32bits(v)
+	this.grow(4)
+	this.byteOrder.PutUint32(this.buf[this.position:], u32)
+	this.position += 4
+	return
+}
+func (this *ByteArray) WriteInt(v int32) {
+	this.grow(4)
+	this.byteOrder.PutUint32(this.buf[this.position:], uint32(v))
+	this.position += 4
+	return
+}
+func (this *ByteArray) WriteShort(v int16) {
+	this.grow(2)
+	this.byteOrder.PutUint16(this.buf[this.position:], uint16(v))
+	this.position += 2
+	return
+}
+func (this *ByteArray) WriteUnsignedInt(v uint32) {
+	this.WriteInt(int32(v))
+	return
+}
+
+func (b *ByteArray) grow(n int) {
+	if b.position+n > cap(b.buf) {
+		var buf []byte
+		if b.buf == nil && n <= len(b.bootstrap) {
+			buf = b.bootstrap[:n]
+		} else {
+			// not enough space anywhere
+			buf = makeSlice(2*cap(b.buf) + n)
+			copy(buf, b.buf[:b.position])
+		}
+		b.buf = buf
+	}
+	m := len(b.buf)
+	if b.position+n >= m {
+		b.buf = b.buf[:b.position+n]
+	}
+}
+
+// makeSlice allocates a slice of size n. If the allocation fails, it panics
+// with ErrTooLarge.
+func makeSlice(n int) []byte {
+	// If the make fails, give a known error.
+	defer func() {
+		if recover() != nil {
+			panic(ErrTooLarge)
+		}
+	}()
+	return make([]byte, n)
+}
---
+++ util/byte_array_test.go
@@ -0,0 +1,116 @@
+package util
+
+import (
+	"testing"
+)
+
+func TestByteArray(t *testing.T) {
+	var err error
+	ba := NewByteArray()
+	ba.WriteByte(0xaa)
+	t.Logf("len:%d", ba.Length())
+	ba.SetPosition(ba.Length() - 1)
+	v, err := ba.ReadByte()
+	if err != nil {
+		t.Fatalf("ba.ReadByte err:%v", err)
+		t.FailNow()
+	}
+	if v != 0xaa {
+		t.Fatalf("v(0x%x) != 0xaa", v)
+		t.FailNow()
+	}
+	t.Logf("len:%d", ba.Length())
+	ba.WriteBoolean(true)
+	ba.SetPosition(ba.Length() - 1)
+	vb, err := ba.ReadBoolean()
+	if err != nil {
+		t.Fatalf("ba.ReadBoolean err:%v", err)
+		t.FailNow()
+	}
+	if !vb {
+		t.Fatalf("v(%v) != true", vb)
+	}
+	t.Logf("len:%d", ba.Length())
+	ba.WriteDouble(0.5)
+	t.Logf("len:%d", ba.Length())
+	ba.SetPosition(ba.Length() - 8)
+	vdb, err := ba.ReadDouble()
+	if err != nil {
+		t.Fatalf("ba.ReadDouble err:%v", err)
+		t.FailNow()
+	}
+	if vdb != 0.5 {
+		t.Fatalf("v(%f) != 0.5", vdb)
+	}
+	ba.WriteFloat(0.6)
+	t.Logf("len:%d", ba.Length())
+	ba.SetPosition(ba.Length() - 4)
+	vf, err := ba.ReadFloat()
+	if err != nil {
+		t.Fatalf("ba.ReadFloat err:%v", err)
+		t.FailNow()
+	}
+	if vf != 0.6 {
+		t.Fatalf("v(%f) != 0.6", vf)
+	}
+	ba.WriteInt(0xaaaaaaa)
+	t.Logf("len:%d", ba.Length())
+	ba.SetPosition(ba.Length() - 4)
+	vn, err := ba.ReadInt()
+	if err != nil {
+		t.Fatalf("ba.ReadInt err:%v", err)
+		t.FailNow()
+	}
+	if vn != 0xaaaaaaa {
+		t.Fatalf("v(0x%x) != 0xaaaaaaaa", vn)
+	}
+	ba.WriteShort(0xaaa)
+	t.Logf("len:%d", ba.Length())
+	ba.SetPosition(ba.Length() - 2)
+	vs, err := ba.ReadShort()
+	if err != nil {
+		t.Fatalf("ba.ReadShort err:%v", err)
+		t.FailNow()
+	}
+	if vs != 0xaaa {
+		t.Fatalf("v(0x%x) != 0xaaa", vs)
+	}
+
+	ba.WriteUnsignedInt(0xaaaaaaa)
+	t.Logf("len:%d", ba.Length())
+	ba.SetPosition(ba.Length() - 4)
+	vun, err := ba.ReadUnsignedInt()
+	if err != nil {
+		t.Fatalf("ba.ReadUnsignedInt err:%v", err)
+		t.FailNow()
+	}
+	if vun != 0xaaaaaaa {
+		t.Fatalf("v(0x%x) != 0xaaaaaaa", vun)
+	}
+	ban := NewByteArray()
+	ban.WriteBytes(ba, 0, 0)
+
+	t.Logf("ban.len:%d", ban.Length())
+	ban.SetPosition(ban.Length() - 4)
+	vun, err = ban.ReadUnsignedInt()
+	if err != nil {
+		t.Fatalf("ban.ReadUnsignedInt err:%v", err)
+		t.FailNow()
+	}
+	if vun != 0xaaaaaaa {
+		t.Fatalf("v(0x%x) != 0xaaaaaaa", vun)
+	}
+
+	ba.ReadBytes(ban, 0, 0)
+
+	t.Logf("ban.len:%d", ban.Length())
+	ban.SetPosition(ba.Length() - 4)
+	vun, err = ban.ReadUnsignedInt()
+	if err != nil {
+		t.Fatalf("ban.ReadUnsignedInt err:%v", err)
+		t.FailNow()
+	}
+	if vun != 0xaaaaaaa {
+		t.Fatalf("v(0x%x) != 0xaaaaaaa", vun)
+	}
+}
